52a6373a069dd6d9d94f368b27d730026462c9cb
Andres Martinez G <me@amartinez.co>
2022-06-25 22:20:18 -0500

    Rdoc documnetation added

--- lib/sudoku_solver/sudoku/strategies/hidden_singles.rb
+++ lib/sudoku_solver/sudoku/strategies/hidden_singles.rb
@@ -1,10 +1,19 @@
+# frozen_string_literal: true
+
+# = hidden_singles.rb
 class Sudoku
   module Strategies
+    # === HiddenSingles class
+    #
+    # It determines if a candidate value for a cell is the last option available on: a matrix, a row or a column
     class HiddenSingles
+      # @param game [Sudoku]
       def initialize(game)
         @game = game
       end
 
+      # It traverses all nodes with one candidate and changes the current value with the only candidate available
+      # It pushes the _id_ of the node on +nodes_changed_ids+ array
       def process
         puts "Executing #{self.class.name}"
 
@@ -16,6 +25,7 @@ class Sudoku
         end
       end
 
+      # @return [Sudoku::Node] nodes which have only one candidate available
       def nodes_with_one_candidate
         @game.graph.vertices.select(&:only_one_candidate?)
       end
